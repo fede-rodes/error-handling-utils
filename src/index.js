f6e262b8f3a2e6f1a01cec86f624147a15724ac7
fede-rodes <rodesf@gmail.com>
2018-08-17 22:49:26 +0200

    removing unnecessary deps

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ ErrorHandling.traverseErrors = (errors, cond) => {
   }
 
   const keys = Object.keys(errors);
-  const length = keys.length;
+  const { length } = keys;
 
   for (let i = 0; i < length; i += 1) {
     // If condition is met, interrupt the for loop and return relevant data
